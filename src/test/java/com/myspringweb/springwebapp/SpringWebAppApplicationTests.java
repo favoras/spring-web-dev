ed3cafa15584ade7ff68d22366a6c90a039d4a63
Edgaras <twixas99@gmail.com>
2020-11-18 01:33:27 +0200

    Added empty project and .gitignore

---
+++ src/test/java/com/myspringweb/springwebapp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringweb.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
